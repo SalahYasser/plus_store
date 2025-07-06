2b0435a96f784e71e784055abf36020b2a21d848
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-07-06 17:51:59 +0300

    create and implement login cubit

---
+++ lib/features/login/presentation/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,49 @@
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit(this.dio) : super(LoginInitial());
+
+  final Dio dio;
+
+  Future<void> login({
+    required String userName,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+
+    try {
+      final response = await dio.post(
+        'https://dummyjson.com/auth/login',
+        data: {"username": userName, "password": password},
+        options: Options(headers: {'Content-Type': 'application/json'}),
+      );
+
+      if (response.statusCode == 200) {
+        emit(LoginSuccess());
+
+        final data = response.data;
+
+        final accessToken = data['accessToken'];
+
+        final pref = await SharedPreferences.getInstance();
+
+        await pref.setString('accessToken', accessToken);
+
+      } else {
+        emit(
+          LoginFailure(
+            errMessage: 'Login failed with status: ${response.statusCode}',
+          ),
+        );
+      }
+    } catch (e) {
+      emit(LoginFailure(errMessage: 'Login failed with error: ${e.toString()}'));
+    }
+
+  }
+}
